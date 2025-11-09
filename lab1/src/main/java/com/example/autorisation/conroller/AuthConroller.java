e306c6890ed4004a3b9b55513341fe8ae4b70361
Natalia <natalia200218@yandex.ru>
2025-11-09 17:33:55 +0400

    добавление подтверждения пароля при входе

--- lab1/src/main/java/com/example/autorisation/conroller/AuthConroller.java
+++ lab1/src/main/java/com/example/autorisation/conroller/AuthConroller.java
@@ -1,9 +1,15 @@
 package com.example.autorisation.conroller;
 
 import com.example.autorisation.repository.AdminRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
 
 @Controller
 public class AuthConroller {
@@ -21,4 +27,18 @@ public class AuthConroller {
         return "auth/login";
     }
 
+    @PostMapping("/login")
+    public void сonfirmPassword(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String password = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirmPassword");
+        if (password == null || confirmPassword == null) {
+            response.sendRedirect("/login?confirmError");
+        }
+        if (!password.equals(confirmPassword)) {
+            response.sendRedirect("/login?confirmError");
+        } else {
+            request.getRequestDispatcher("/login-process").forward(request, response);
+        }
+
+    }
 }
--- lab1/src/main/java/com/example/autorisation/security/SecurityConfig.java
+++ lab1/src/main/java/com/example/autorisation/security/SecurityConfig.java
@@ -44,6 +44,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
+                        .loginProcessingUrl("/login-process")
                         .successHandler(loginSuccessHandler)
                         .failureHandler(loginFailureHandler)
                         .permitAll()
